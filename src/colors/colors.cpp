83343fcac288a0bcc4405793b8d7882078430d6b
JakeKolk <54716860+JakevdKolk@users.noreply.github.com>
2025-06-06 12:59:46 +0200

    Added beter color config (#2)
    * added beter color config
    
    * added party member config
    
    * updated color based of player feedback
    
    * made colors non static
    
    * changed setup to color class
    
    * made the constructor explicit
    
    * changed stuff arround and tested warning given

---
+++ src/colors/colors.cpp
@@ -0,0 +1,83 @@
+#include "colors.h"
+
+int rgbLED[3] = {12, 13, 14}; // Define RGB LED pins globally
+
+Colors::Colors(int r, int g, int b) : redChannel(r), greenChannel(g), blueChannel(b)
+{
+    ledcSetup(redChannel, 5000, 8);
+    ledcSetup(greenChannel, 5000, 8);
+    ledcSetup(blueChannel, 5000, 8);
+
+    ledcAttachPin(rgbLED[0], redChannel);
+    ledcAttachPin(rgbLED[1], greenChannel);
+    ledcAttachPin(rgbLED[2], blueChannel);
+}
+
+void Colors::SetColor(int r, int g, int b)
+{
+    ledcWrite(redChannel, 255 - r);
+    ledcWrite(greenChannel, 255 - g);
+    ledcWrite(blueChannel, 255 - b);
+}
+
+void Colors::resetColor()
+{
+    SetColor(0, 0, 0);
+}
+
+void Colors::pulse_rgbLED(colorCodes color, int loopCount)
+{
+    for (int i = 0; i < loopCount; ++i)
+    {
+        applyColor(color);
+        delay(500);
+        resetColor();
+        delay(500);
+    }
+}
+
+void Colors::applyColor(colorCodes color)
+{
+    switch (color)
+    {
+    case colorCodes::Red:
+        SetColor(255, 0, 0);
+        break;
+    case colorCodes::Green:
+        SetColor(0, 255, 0);
+        break;
+    case colorCodes::Blue:
+        SetColor(0, 0, 255);
+        break;
+    case colorCodes::Yellow:
+        SetColor(255, 255, 0);
+        break;
+    case colorCodes::Cyan:
+        SetColor(0, 255, 255);
+        break;
+    case colorCodes::Magenta:
+        SetColor(255, 0, 255);
+        break;
+    case colorCodes::White:
+        SetColor(255, 255, 255);
+        break;
+    case colorCodes::Aether:
+        SetColor(85, 235, 235);
+        break;
+    case colorCodes::Lucian:
+        SetColor(35, 0, 90);
+        break;
+    case colorCodes::Jiji:
+        SetColor(255, 75, 5);
+        break;
+    case colorCodes::Taron:
+        SetColor(140, 28, 171);
+        break;
+    case colorCodes::Seraphine:
+        SetColor(172, 39, 44);
+        break;
+    default:
+        resetColor();
+        break;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,28 @@
 #include <Arduino.h>
+#include "colors/colors.h"
+
+Colors colorHandler;
 
 int LEDPins[4] = {4, 5, 16, 17};
 
-int rgbLED[3] = {12, 13, 14};
+colorCodes partyColors[] = {
+    colorCodes::Aether,
+    colorCodes::Jiji,
+    colorCodes::Seraphine,
+    colorCodes::Taron,
+    colorCodes::Lucian};
 
-#define RED_CHANNEL 0
-#define GREEN_CHANNEL 1
-#define BLUE_CHANNEL 2
-
-// Setup function
 void setup()
 {
   for (int i = 0; i < 4; i++)
   {
     pinMode(LEDPins[i], OUTPUT);
   }
-
-  ledcSetup(RED_CHANNEL, 5000, 8);
-  ledcSetup(GREEN_CHANNEL, 5000, 8);
-  ledcSetup(BLUE_CHANNEL, 5000, 8);
-
-  ledcAttachPin(rgbLED[0], RED_CHANNEL);
-  ledcAttachPin(rgbLED[1], GREEN_CHANNEL);
-  ledcAttachPin(rgbLED[2], BLUE_CHANNEL);
 }
 
-void SetColor(int r, int g, int b)
-{
-  ledcWrite(RED_CHANNEL, 255 - r);
-  ledcWrite(GREEN_CHANNEL, 255 - g);
-  ledcWrite(BLUE_CHANNEL, 255 - b);
-}
-
-// Main loop
 void loop()
 {
-  int direction[4] = {0, 1, 2, 3}; // 0=N, 1=E, 2=S, 3=W
+  int direction[4] = {0, 1, 2, 3};
 
   for (int i = 0; i < 4; i++)
   {
@@ -43,20 +30,10 @@ void loop()
     {
       digitalWrite(LEDPins[j], j == direction[i] ? LOW : HIGH);
     }
+  }
 
-    SetColor(255, 0, 0);
-    delay(1000);
-    SetColor(0, 255, 0);
-    delay(1000);
-    SetColor(0, 0, 255);
-    delay(5000);
-    SetColor(255, 255, 0);
-    delay(1000);
-    SetColor(0, 255, 255);
-    delay(1000);
-    SetColor(255, 0, 255);
-    delay(1000);
-    SetColor(255, 255, 255);
-    delay(5000);
+  for (int i = 0; i < 5; i++)
+  {
+    colorHandler.pulse_rgbLED(partyColors[i], 1);
   }
 }

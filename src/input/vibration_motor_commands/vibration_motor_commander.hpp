87eaf79027b3efbf5ffde2d7864a97e7c1ab7f4d
Jake <54716860+JakevdKolk@users.noreply.github.com>
2025-06-14 19:36:05 +0200

    fixed constructor mismatch

--- src/input/vibration_motor_commands/vibration_motor_commander.hpp
+++ src/input/vibration_motor_commands/vibration_motor_commander.hpp
@@ -10,6 +10,6 @@ private:
     vibration_statuses statuses_;
 
 public:
-    VibrationMotorCommands(vibration_context *context_, vibration_state *state_, vibration_statuses statuses);
+    VibrationMotorCommands(vibration_context *context, vibration_state *state, vibration_statuses statuses);
     void Execute(String args) const override;
 };
